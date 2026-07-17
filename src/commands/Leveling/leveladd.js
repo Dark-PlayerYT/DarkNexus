3708ef548c0e2e82f517a7ef474ea6109648ecb5
Dark-PlayerYT <140445838+Dark-PlayerYT@users.noreply.github.com>
2026-07-17 15:49:18 +0300

    Update leveladd.js

--- src/commands/Leveling/leveladd.js
+++ src/commands/Leveling/leveladd.js
@@ -4,78 +4,82 @@ import { TitanBotError, ErrorTypes } from '../../utils/errorHandler.js';
 import { checkUserPermissions } from '../../utils/permissionGuard.js';
 import { addLevels, getLevelingConfig } from '../../services/leveling/leveling.js';
 import { createEmbed } from '../../utils/embeds.js';
-
 import { InteractionHelper } from '../../utils/interactionHelper.js';
+
 export default {
   data: new SlashCommandBuilder()
-    .setName('leveladd')
-    .setDescription('Add levels to a user')
+    .setName('seviye-ekle') // Komut ismi Türkçe yapıldı
+    .setDescription('Bir kullanıcıya seviye ekler')
     .addUserOption((option) =>
       option
-        .setName('user')
-        .setDescription('The user to add levels to')
+        .setName('kullanıcı')
+        .setDescription('Seviye eklenecek kullanıcı')
         .setRequired(true)
     )
     .addIntegerOption((option) =>
       option
-        .setName('levels')
-        .setDescription('Number of levels to add')
+        .setName('miktar')
+        .setDescription('Eklenecek seviye miktarı')
         .setRequired(true)
         .setMinValue(1)
     )
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
     .setDMPermission(false),
-  category: 'Leveling',
+  category: 'Seviye',
 
   async execute(interaction, config, client) {
-    await InteractionHelper.safeDefer(interaction);
-
+    // 1. Önce yetki kontrolü yapıyoruz (Defer etmeden önce, yetki yoksa doğrudan gizli cevap dönebilsin)
     const hasPermission = await checkUserPermissions(
       interaction,
       PermissionFlagsBits.ManageGuild,
-      'You need ManageGuild permission to use this command.'
+      'Bu komutu kullanabilmek için Sunucuyu Yönet yetkisine sahip olmalısınız.'
     );
     if (!hasPermission) return;
 
+    // 2. Seviye sisteminin durumunu kontrol ediyoruz
     const levelingConfig = await getLevelingConfig(client, interaction.guildId);
     if (!levelingConfig?.enabled) {
-      await InteractionHelper.safeEditReply(interaction, {
+      await interaction.reply({
         embeds: [
           new EmbedBuilder()
             .setColor('#f1c40f')
-            .setDescription('The leveling system is currently disabled on this server.')
+            .setDescription('Seviye sistemi şu anda bu sunucuda devre dışı bırakılmış.')
         ],
         flags: MessageFlags.Ephemeral
       });
       return;
     }
 
-    const targetUser = interaction.options.getUser('user');
-    const levelsToAdd = interaction.options.getInteger('levels');
+    // 3. Kontroller başarıyla geçtiyse, veritabanı işlemi sürebileceği için şimdi güvenli şekilde defer ediyoruz
+    await InteractionHelper.safeDefer(interaction);
+
+    const targetUser = interaction.options.getUser('kullanıcı');
+    const levelsToAdd = interaction.options.getInteger('miktar');
 
     const member = await interaction.guild.members.fetch(targetUser.id).catch(() => null);
     if (!member) {
       throw new TitanBotError(
-        `User ${targetUser.id} not found in this guild`,
+        `Kullanıcı ${targetUser.id} sunucuda bulunamadı`,
         ErrorTypes.USER_INPUT,
-        'The specified user is not in this server.'
+        'Belirtilen kullanıcı bu sunucuda bulunmuyor.'
       );
     }
 
+    // Seviye ekleme servisini çağırıyoruz
     const userData = await addLevels(client, interaction.guildId, targetUser.id, levelsToAdd);
 
     await InteractionHelper.safeEditReply(interaction, {
       embeds: [
         createEmbed({
-          title: 'Levels Added',
-          description: `Successfully added ${levelsToAdd} levels to ${targetUser.tag}.\n**New Level:** ${userData.level}`,
+          title: 'Seviye Eklendi',
+          description: `${targetUser} kullanıcısına başarıyla **${levelsToAdd}** seviye eklendi.\n**Yeni Seviyesi:** ${userData.level}`,
           color: 'success'
         })
       ]
     });
 
     logger.info(
-      `[ADMIN] User ${interaction.user.tag} added ${levelsToAdd} levels to ${targetUser.tag} in guild ${interaction.guildId}`
+      `[ADMIN] ${interaction.user.tag}, ${interaction.guildId} sunucusunda ${targetUser.tag} kullanıcısına ${levelsToAdd} seviye ekledi.`
     );
   }
 };
